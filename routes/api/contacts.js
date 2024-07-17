c6d4b142e4b1692b60faed977e2fc8ed4a29e69c
Wiola-D <wioletta.d.work@gmail.com>
2024-07-17 14:14:47 +0200

    Update routers in api/contacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,40 +4,18 @@ const router = express.Router();
 
 const {
   getAllContacts,
-  //   getContact,
-  //   createContact,
-  //   patchContact,
-  //   putContact,
-  //   deleteContact,
+  getContact,
+  createContact,
+  patchContact,
+  putContact,
+    deleteContact,
 } = require("../../controllers/index");
 
 router.get("/contacts", getAllContacts);
-// router.get("/contacts:id", getContact);
-// router.post("/contacts/", createContact);
-// router.put("/contacts/:id", putContact);
-// router.patch("/contacts/:id", patchContact);
-// router.delete("/contacts/:id", deleteContact);
-
-// router.get("/", async (req, res, next) => {
-//   res.json({ message: "helllllo" });
-// });
-
-// router.get("/:contactId", async (req, res, next) => {
-//   res.json({ message: "template message" });
-// });
-
-// router.post("/", async (req, res, next) => {
-//   res.json({ message: "template message" });
-// });
-
-// router.delete("/:contactId", async (req, res, next) => {
-//   res.json({ message: "template message" });
-// });
-
-// router.put("/:contactId", async (req, res, next) => {
-//   res.json({ message: "template message" });
-// });
+router.get("/contacts/:id", getContact);
+router.post("/contacts/", createContact);
+router.put("/contacts/:id", putContact);
+router.patch("/contacts/:id", patchContact);
+router.delete("/contacts/:id", deleteContact);
 
 module.exports = router;
-
-//hello
