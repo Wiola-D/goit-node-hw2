2537de427de20a9bf6c933042d4d3127ea37895b
Wiola-D <wioletta.d.work@gmail.com>
2024-07-17 14:15:38 +0200

    Add functions in controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,88 +1,89 @@
 const {
   fetchContacts,
-  // fetchContact,
-  // insertContact,
-  // updateContact,
-  // removeContact,
+  fetchContact,
+  insertContact,
+  updateContact,
+  removeContact,
 } = require("./services");
 
 const getAllContacts = async (req, res, next) => {
   try {
-    const tasks = await fetchContacts();
-    res.json(tasks);
+    const contacts = await fetchContacts();
+    res.status(200).json({ contacts });
   } catch (error) {
     next(error);
   }
 };
 
-// const getContact = async (req, res, next) => {
-//   try {
-//     const task = await fetchContact(req.params.id);
-//     if (task) {
-//       res.json({
-//         ...task.toObject(),
-//         html: task.htmlify(),
-//       });
-//     } else {
-//       next();
-//     }
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+const getContact = async (req, res, next) => {
+  try {
+    const contact = await fetchContact(req.params.id);
+    if (contact) {
+      res.json({
+        ...contact.toObject(),
+        html: contact.htmlify(),
+      });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
-// const createContact = async (req, res, next) => {
-//   const { name, email, phone, favorite } = req.body;
-//   try {
-//     const result = await insertContact({ name, email, phone, favorite });
-//     res.status(201).json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+const createContact = async (req, res, next) => {
+  const { name, email, phone, favorite } = req.body;
+  try {
+    const result = await insertContact({ name, email, phone, favorite });
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-// const putContact = async (req, res, next) => {
-//   const { id } = req.params;
-//   try {
-//     const result = await updateContact({
-//       id,
-//       toUpdate: req.body,
-//       upsert: true,
-//     });
-//     res.json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-// const patchContact = async (req, res, next) => {
-//   const { id } = req.params;
-//   try {
-//     const result = await updateContact({ id, toUpdate: req.body });
-//     if (!result) {
-//       next();
-//     } else {
-//       res.json(result);
-//     }
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+const putContact = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const result = await updateContact({
+      id,
+      toUpdate: req.body,
+      upsert: true,
+    });
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-// const deleteContact = async (req, res, next) => {
-//   const { id } = req.params;
-//   try {
-//     await removeContact(id);
-//     res.status(204).send({ message: "Task deleted" });
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+const patchContact = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const result = await updateContact({ id, toUpdate: req.body });
+    if (!result) {
+      next();
+    } else {
+      res.json(result);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteContact = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    await removeContact(id);
+    res.status(200).send("Task deleted");
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   getAllContacts,
-  // getContact,
-  // createContact,
-  // patchContact,
-  // putContact,
-  // deleteContact,
+  getContact,
+  createContact,
+  patchContact,
+  putContact,
+  deleteContact,
 };
