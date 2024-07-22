a763933c4a4a587dfb04efdba605954c211fb5cb
Wiola-D <wioletta.d.work@gmail.com>
2024-07-22 14:24:16 +0200

    Create userRouter / register

--- routes/api/authRouter.js
+++ routes/api/authRouter.js
@@ -1,12 +1,29 @@
 const express = require("express");
-
+const User = require("../../models/usersSchema");
 const router = express.Router();
 
-router.use("/register", async (req, res, next) => {
-  res.json("hello by register");
+router.get("/", (req, res) => {
+  res.json("something");
 });
 
-router.use("/login", async (req, res, next) => {
+router.post("/register", async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email }, { _id: 1 }).lean();
+
+  if (user) {
+    return res.status(409).json({ message: "This email is already taken." });
+  }
+  try {
+    const newUser = new User({ email });
+    await newUser.setPassword(password);
+    await newUser.save();
+    return res.status(201).json({ message: "Created" });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
   res.json("hello by login");
 });
 
