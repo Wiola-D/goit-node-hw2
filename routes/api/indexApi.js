51a1a3e5241ce3f3854ec2c2f9f7b46fa56d20c0
Wiola-D <wioletta.d.work@gmail.com>
2024-07-22 21:55:32 +0200

    Change authRouter on userRouter, add logout/current

--- routes/api/indexApi.js
+++ routes/api/indexApi.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const contactsRouter = require("./contactsRouter");
-const authMiddleware = require("../../middelware/jwt");
+const authMiddleware = require("../../middelware/auth");
 
 const router = express.Router();
 
-const authRouter = require("./authRouter");
+const authRouter = require("./usersRouter");
 
-router.use("/auth", authRouter);
+router.use("/users", authRouter);
 router.use("/contacts", authMiddleware, contactsRouter);
 
 module.exports = router;
---
+++ routes/api/usersRouter.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const User = require("../../models/usersSchema");
+const jwt = require("jsonwebtoken");
+const authMiddleware = require("../../middelware/auth");
+const getAllUsers = require("../../controllers/userController");
+
+const router = express.Router();
+
+// Endpoint do uzyskiwania wszystkich użytkowników
+router.get("/", getAllUsers);
+
+// Endpoint do rejestracji użytkownika
+router.post("/signup", async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email }).lean();
+
+  if (user) {
+    return res.status(409).json({ message: "This email is already taken." });
+  }
+  try {
+    const newUser = new User({ email });
+    await newUser.setPassword(password);
+    await newUser.save();
+    return res.status(201).json({
+      message: "Create a account",
+      email: newUser.email,
+      subscription: newUser.subscription,
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+// Endpoint do logowania użytkownika
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json({ message: "No such user" });
+  }
+  const isPasswordCorrect = await user.validatePassword(password);
+
+  if (isPasswordCorrect) {
+    const payload = {
+      id: user._id,
+      username: user.username,
+    };
+
+    const token = jwt.sign(payload, process.env.SECRET, { expiresIn: "12h" });
+    user.token = token;
+    await user.save();
+    return res.status(200).json({
+      message: "Login",
+      token,
+      email: user.email,
+      subscription: user.subscription,
+    });
+  } else {
+    return res.status(401).json({ message: "Wrong password" });
+  }
+});
+
+// Endpoint do wylogowania
+router.get("/logout", authMiddleware, async (req, res) => {
+  try {
+    const userId = req.user._id; // id zalogowanego użytkownika
+
+    // Znajdź użytkownika
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+
+    // Usuń token z obiektu użytkownika
+    console.log(user);
+    user.token = null;
+    await user.save();
+
+    return res.status(204).send();
+  } catch (error) {
+    return res.status(401).json({ message: "Not authorized" });
+  }
+});
+
+//Endpoint do pobrania danych aktualnego użytkownika
+router.get("/current", authMiddleware, async (req, res) => {
+  const user = await User.findById(req.user._id);
+  console.log(`current:${user}`);
+  return res.status(200).json({
+    user: {
+      email: req.user.email,
+      subscription: req.user.subscription,
+    },
+  });
+});
+
+module.exports = router;
