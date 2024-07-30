31fc8b3ba339a2272ec3780f3ca90db59d0f24d2
Wiola-D <wioletta.d.work@gmail.com>
2024-07-30 21:46:03 +0200

    Update usersSchema.js

--- models/usersSchema.js
+++ models/usersSchema.js
@@ -20,6 +20,17 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: null,
   },
+  avatarURL: {
+    type: String,
+  },
+  verify: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+    required: [true, "Verify token is required"],
+  },
 });
 
 userSchema.methods.setPassword = async function (password) {
