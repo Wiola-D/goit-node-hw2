747da709921d0d6095a9a6a95363b367eb8cafaa
Wiola-D <wioletta.d.work@gmail.com>
2024-07-31 11:35:19 +0200

    Update verification.js

--- middelware/verification.js
+++ middelware/verification.js
@@ -1,7 +1,14 @@
 const User = require("../models/usersSchema");
 
 const checkVerification = async (req, res, next) => {
-  const user = await User.findOne(req.params.id);
+  const { email } = req.body;
+  const user = await User.findOne({ email: email });
+
+  console.log(user);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
 
   if (!user.verify) {
     return res.status(403).json({ message: "Email not verified" });
